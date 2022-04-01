a8e874290e5e475c5fb1c00b642fb64d288ab3d2
arseniy <aoart2006@yandex.ru>
2022-04-01 03:07:16 +0300

    commit 5 continuing of coding telegram

--- main.py
+++ main.py
@@ -26,6 +26,11 @@ food_recipes_callback = CallbackData("Food_recipes", "iterator")
 country_recipes_callback = CallbackData("Country_recipes", "letter")
 photo_recipes_callback = CallbackData("Country_recipes", "href", "page")
 
+headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
+}
+
 
 @dp.message_handler(commands=['help'])
 async def helping(message: types.Message):
@@ -174,7 +179,8 @@ async def prv_page(query: CallbackQuery):
                                  callback_data=f"nxt:{data + 1}:{query.data.split(':')[2]}"),
         )
         await query.message.edit_text(
-            f"Выберете продукт, из которого вы хотите приготовить блюдо{emoji.emojize(':arrow_down:', language='alias')}",
+            f"Выберете продукт,\
+из которого вы хотите приготовить блюдо{emoji.emojize(':arrow_down:', language='alias')}",
             reply_markup=button_markup)
 
 
@@ -200,7 +206,8 @@ async def nxt_page(query: CallbackQuery):
                                  callback_data=f"nxt:{data + 1}:{query.data.split(':')[2]}"),
         )
         await query.message.edit_text(
-            f"Выберете продукт, из которого вы хотите приготовить блюдо{emoji.emojize(':arrow_down:', language='alias')}",
+            f"Выберете продукт,\
+из которого вы хотите приготовить блюдо{emoji.emojize(':arrow_down:', language='alias')}",
             reply_markup=button_markup)
 
 
@@ -227,10 +234,6 @@ async def send_answer(query: CallbackQuery, callback_data: dict):
 @dp.callback_query_handler(show_type_recipes_callback.filter())
 async def send_answer(query: CallbackQuery, callback_data: dict):
     button_markup = InlineKeyboardMarkup()
-    headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
-    }
     for elem in get_cards(f"https://www.russianfood.com/recipes/bytype/?{callback_data.get('group_name')}", 1, headers):
         button_markup.add(types.InlineKeyboardButton(f'{elem["name"]}', callback_data=photo_recipes_callback.new(
             page=1, href=f'{elem["href"].split("?rid=")[1]}')))
@@ -250,10 +253,6 @@ async def prev_page(query: CallbackQuery):
     data = int(query.data.split(":")[1])
     if data > 0:
         button_markup = InlineKeyboardMarkup()
-        headers = {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
-        }
         for elem in get_cards(f"https://www.russianfood.com/recipes/bytype/?{query.data.split(':')[2]}", data, headers):
             button_markup.add(types.InlineKeyboardButton(f'{elem["name"]}', callback_data=photo_recipes_callback.new(
                 page=data, href=f'{elem["href"].split("?rid=")[1]}')))
@@ -273,10 +272,6 @@ async def next_page(query: CallbackQuery):
     await query.answer()
     button_markup = InlineKeyboardMarkup()
     data = int(query.data.split(":")[1])
-    headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
-    }
     for elem in get_cards(f"https://www.russianfood.com/recipes/bytype/?{query.data.split(':')[2]}", data, headers):
         button_markup.add(types.InlineKeyboardButton(f'{elem["name"]}', callback_data=photo_recipes_callback.new(
             page=data, href=f'{elem["href"].split("?rid=")[1]}')))
@@ -292,10 +287,6 @@ async def next_page(query: CallbackQuery):
 
 @dp.callback_query_handler(photo_recipes_callback.filter())
 async def send_answer(query: CallbackQuery, callback_data: dict):
-    headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36'
-    }
     lst = get_card(f"https://www.russianfood.com/recipes/recipe.php?rid={callback_data['href']}", headers)
     await bot.send_photo(query.message.chat.id, photo=f"{lst['img']}")
     await query.message.answer(f"{lst['description']}\n")
